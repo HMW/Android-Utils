950e81615c5ce65f692514f13e5263b806d4cbdd
HMW <wuheimin@gmail.com>
2017-06-18 19:40:59 +0800

    Remove generated comments

--- utils/src/test/java/com/jajinba/android/utils/ArrayUtilsUnitTest.java
+++ utils/src/test/java/com/jajinba/android/utils/ArrayUtilsUnitTest.java
@@ -8,11 +8,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ArrayUtilsUnitTest {
 
   private Integer[] intArraySizeOne = {0};
