ecda72c16341b5537b6eefcb236e8b5d126ed87f
HMW <wuheimin@gmail.com>
2017-06-18 16:54:18 +0800

    Add ArrayUtils methods

--- utils/src/main/java/com/jajinba/android/utils/ArrayUtils.java
+++ utils/src/main/java/com/jajinba/android/utils/ArrayUtils.java
@@ -1,6 +1,15 @@
 package com.jajinba.android.utils;
 
 
+import java.util.Collection;
+
 public class ArrayUtils {
 
+  public static int getLengthSafe(Object[] array) {
+    return array == null ? 0 : array.length;
+  }
+
+  public static int getLengthSafe(Collection<?> list) {
+    return list == null ? 0 : list.size();
+  }
 }
